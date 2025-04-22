64eca91236216dbb3e117215606cda6f777938eb
ann86 <ann86@LAPTOP-GUIKJJIL>
2025-04-22 18:40:17 +0300

    создана страница для добавления работы

---
+++ Car_Service/Pages/WorkAddPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Car_Service.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для WorkAddPage.xaml
+    /// </summary>
+    public partial class WorkAddPage : Page
+    {
+        public WorkAddPage(Work selectedWork)
+        {
+            InitializeComponent();
+            if (selectedWork != null) _work = selectedWork;
+
+            NumOrder.ItemsSource = Entities.GetContext().Orders.ToList();
+            Position.ItemsSource = Entities.GetContext().Employees.ToList();
+            sName.ItemsSource = Entities.GetContext().Services.ToList();
+            pName.ItemsSource = Entities.GetContext().Parts.ToList();
+        }
+
+        private Work _work = new Work();
+
+        private void bSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (NumOrder.SelectedItem == null) errors.AppendLine("Выберите проблему");
+            if (Position.SelectedItem == null) errors.AppendLine("Выберите сотрудника");
+            if (sName.SelectedItem == null) errors.AppendLine("Выберите услугу");
+            if (pName.SelectedItem == null) errors.AppendLine("Выберите запчасть");
+
+            if (errors.Length > 0) { MessageBox.Show(errors.ToString()); return; }
+
+            try
+            {
+                Entities.GetContext().SaveChanges();
+                MessageBox.Show("Данные успешно сохранены");
+                NavigationService.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void bBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
--- Car_Service/Pages/WorkPage.xaml.cs
+++ Car_Service/Pages/WorkPage.xaml.cs
@@ -33,17 +33,33 @@ namespace Car_Service.Pages
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            //NavigationService.Navigate(new WorkAddPage);
+            NavigationService.Navigate(new WorkAddPage(null));
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
+            var workForRemoving = DGridWork.SelectedItems.Cast<Work>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить записи в количестве {workForRemoving.Count()} элементов?",
+                "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    Entities.GetContext().Work.RemoveRange(workForRemoving);
+                    Entities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные успешно удалены!");
 
+                    DGridWork.ItemsSource = Entities.GetContext().Work.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
 
         private void BtnRed_Click(object sender, RoutedEventArgs e)
         {
-
+            NavigationService.Navigate(new WorkAddPage((sender as Button).DataContext as Work));
         }
     }
 }
